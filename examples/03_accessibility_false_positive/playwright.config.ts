52d1f3f4344ebe81712bf53dca0d31ffe6f27b2b
Filip Hric <filip@filiphric.sk>
2024-05-09 15:21:24 +0200

    add crm project

---
+++ examples/03_accessibility_false_positive/playwright.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from '@playwright/test';
+import {
+  createReplayReporterConfig,
+  devices as replayDevices,
+} from "@replayio/playwright";
+import { config } from 'dotenv'
+
+config({ path: '../../.env' })
+
+export default defineConfig({
+  testDir: './playwright',
+  reporter: [
+    createReplayReporterConfig({
+      upload: true,
+      apiKey: process.env.REPLAY_API_KEY,
+    }),
+    ['line']
+  ],
+  use: {
+    baseURL: 'http://localhost:3003',
+    trace: 'off',
+  },
+  projects: [
+    {
+      name: 'replay-chromium',
+      use: { ...(replayDevices['Replay Chromium']) },
+    },
+  ],
+});
